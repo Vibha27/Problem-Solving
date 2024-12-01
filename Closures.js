9392ca96c9d66fcf76cb1b32cc8e9d4dba678253
Vibha27 <svibha081@gmail.com>
2024-12-01 20:59:27 +0530

    implemented closures example

---
+++ Closures.js
@@ -0,0 +1,11 @@
+function counter () {
+    let count=0;
+    return () => {
+        count++;
+        return count;
+    }
+}
+
+const count = counter();
+console.log(count())
+console.log(count())
