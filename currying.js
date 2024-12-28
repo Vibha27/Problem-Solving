b45e4e8c665a146d3824e221fd2179639b079c03
Vibha27 <svibha081@gmail.com>
2024-12-28 22:04:48 +0530

    implemented currying function for addition and other arithemetic operation. implemented curried function for dynamic argument length and different combinaton of calling function

--- currying.js
+++ currying.js
@@ -13,3 +13,70 @@ function sum (num1) {
 
 console.log(sum(2)(4)(5))
 console.log(sum(2)(4)(5))
+
+function add (a) {
+    return function (b) {
+        return a+b
+    }
+}
+
+console.log(add(2)(5))
+
+function evaluate (operation) {
+    return function (a) {
+        return function (b) {
+            if (operation === "mul") return a*b;
+            else if (operation === "add") return a+b;
+            else if (operation === "sub") return a-b;
+            else if (operation === "divide") return a/b;
+            else return "Invalid"
+        }
+    }
+}
+
+console.log(evaluate("add")(5)(6))
+
+// infinite currying addInfinite(2)(3)(6)() => cars24 question
+
+function addInfinite (a) {
+    return function (b) {
+        if (b) return addInfinite(a+b)
+        return a
+    }
+}
+console.log(addInfinite(2)(3)(6)())
+
+// partial application
+function partialAdd (a) {
+    return function (b,c) {
+        return a+b+c
+    }
+}
+console.log(partialAdd(4)(8,9))
+
+// convert normal function to curried function
+function curry(func) {
+
+}
+const addFunction = (a,b,c,d) => a+b+c+d
+const curriedFunc = curry(addFunction)
+
+// currying function accept any no. of args with different combination => Myntra question
+function curry(fn) {
+    // your code here
+    const recursiveFn = (...args) => {
+      if (args.length >= fn.length) return fn(...args)
+    //   return recursiveFn.bind(this, ...args)
+      return (...args2) => recursiveFn(...args, ...args2)
+    }
+    return recursiveFn
+  }
+  
+  const join = (a, b, c) => {
+     return `${a}_${b}_${c}`
+  }
+  const curriedJoin = curry(join)
+  console.log(curriedJoin(1, 2, 3)) // '1_2_3'
+  console.log(curriedJoin(1)(2, 3)) // '1_2_3'
+  console.log(curriedJoin(1, 2)(3)) // '1_2_3'
+  
