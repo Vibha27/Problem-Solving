ff33b9457fd01c4a6222319870c1d46b2dc8c7b4
Vibha27 <svibha081@gmail.com>
2024-12-29 16:35:16 +0530

    implemented memoization with resolver function that accepts function to be run on args to specify how key should be created and return cached result

--- memoization.js
+++ memoization.js
@@ -8,24 +8,51 @@ original function using func.apply(this, args) to preserve the correct context a
 We store the result in the cache and return it.
  *  */
 function memoizeFn(fn) {
-    let cacheData = {};
-    return (...args) => {
-        let key = JSON.stringify(args)
+  let cacheData = {};
+  return (...args) => {
+    let key = JSON.stringify(args);
 
-        if(key in cacheData) {
-            return cacheData[key]
-        } else {
-            cacheData[key] = fn.apply(this,args);
-            return cacheData[key]
-        }
+    if (key in cacheData) {
+      return cacheData[key];
+    } else {
+      cacheData[key] = fn.apply(this, args);
+      return cacheData[key];
     }
+  };
 }
 
-function add(num1, num2) { 
-    return num1 + num2 
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-const result = memoizeFn(add)
-console.log(result(2,3))
-console.log(result(2,3))
+const result = memoizeFn(add);
+console.log(result(2, 3));
+console.log(result(2, 3));
 
+
+// with resolver function that accepts function to be run on args to specify how key should be created
+function memo(func, resolver = (...args) => args.join("_")) {
+  // your code here
+  let cache = {};
+
+  return function (...args) {
+    let str_args = resolver(...args);
+    if (str_args in cache) {
+      return cache[str_args];
+    } else {
+      cache[str_args] = func.apply(this, args);
+      return cache[str_args];
+    }
+  };
+}
+
+const func = (arg1, arg2) => {
+  return arg1 + arg2;
+};
+const memoed = memo(func);
+console.log(memoed(1, 2));
+// 3, func is called
+console.log(memoed(1, 2));
+// 3 is returned right away without calling func
+console.log(memoed(1, 3));
+// 4, new arguments, so func is called
