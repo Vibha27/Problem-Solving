4c2f7be79c80f723b0072a541f7c4b6ce323475d
Vibha27 <svibha081@gmail.com>
2025-02-13 21:28:49 +0530

    implemented reversed string without using any built in method

---
+++ ReverseString.js
@@ -0,0 +1,14 @@
+// without using any built-in method
+
+function reverseString(str) {
+  let reverseString = "";
+  
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseString += str[i];
+  }
+
+  return reverseString;
+}
+
+const str = "Vibha Singh";
+console.log(reverseString(str));
