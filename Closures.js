2fa3aefe07c1636df8ff6560f666260091cfaa04
Vibha27 <svibha081@gmail.com>
2025-05-01 19:43:29 +0530

    implemented debouncing using closure concept

--- Closures.js
+++ Closures.js
@@ -9,3 +9,21 @@ function counter () {
 const count = counter();
 console.log(count())
 console.log(count())
+
+
+function debounce (fn, delay) {
+    let timer;
+
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => fn(...args), delay)
+    }
+}
+
+const log = () => console.log("Triggered!");
+const debouncedLog = debounce(log, 500);
+
+debouncedLog(); // won't run immediately
+debouncedLog(); // cancels previous
+debouncedLog(); // cancels previous
+setTimeout(debouncedLog, 600); // ✅ runs once after 600ms
